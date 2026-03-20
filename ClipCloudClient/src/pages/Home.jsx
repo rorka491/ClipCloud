c82f2597ea7cb8a72994aa053ef6f0855716acb0
Daniil Riazanov <ryazanow45@gmail.com>
2026-03-20 15:17:45 +0100

    Improve Home page responsiveness for different screen sizes

--- ClipCloudClient/src/pages/Home.jsx
+++ ClipCloudClient/src/pages/Home.jsx
@@ -16,24 +16,34 @@ export default function Home() {
     return (
         <div className="bg-blue-400/70 flex w-full h-screen justify-center items-center relative">
 
-            <div className="fixed rounded-2xl top-5 left-[15%] w-full md:w-[70%] h-30 bg-white flex items-center justify-center shadow z-50">
+            <div className="fixed top-3 left-1/2 -translate-x-1/2 
+                w-[90%] md:w-[80%] lg:w-[70%] 
+                 h-30 
+                bg-white rounded-2xl flex items-center justify-center shadow z-50">
                 Реклама
             </div>
 
-            <div className="fixed rounded-2xl bottom-5 left-[15%] w-full md:w-[70%] h-30 bg-white flex items-center justify-center shadow z-50">
+            <div className="fixed bottom-3 left-1/2 -translate-x-1/2 
+                w-[90%] md:w-[80%] lg:w-[70%] 
+                h-30 
+                bg-white rounded-2xl flex items-center justify-center shadow z-50">
                 Реклама
             </div>
 
-            <div className="fixed rounded-2xl top-5 bottom-5 left-3 w-50 bg-white flex items-center justify-center shadow z-50">
+            <div className="hidden lg:flex fixed top-5 bottom-5 left-3 
+                w-40 xl:w-48 
+                bg-white rounded-2xl items-center justify-center shadow z-50">
                 Реклама
             </div>
 
-            <div className="fixed rounded-2xl top-5 bottom-5 right-3 w-50 bg-white flex items-center justify-center shadow z-50">
+            <div className="hidden lg:flex fixed top-5 bottom-5 right-3 
+                w-40 xl:w-48 
+                bg-white rounded-2xl items-center justify-center shadow z-50">
                 Реклама
             </div>
 
             <div className="flex flex-col gap-2 w-full md:w-sm px-15 
-                            ml-32 mr-32 mt-16 mb-16">
+                            md:ml-32 md:mr-32 md:mt-16 md:mb-16">
 
                 <button
                     className="bg-white hover:text-white cursor-pointer hover:bg-black/25 transition duration-200 h-8 rounded-2xl w-full"
